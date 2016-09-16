74f92f3ac05ac90d8fcb1ac0a8f3679b4a2b76f7
Dave Schukin <daveschukin@gmail.com>
2016-09-15 22:14:33 -0700

    v1.3.0

--- Buglife.framework/Versions/A/Headers/Buglife.h
+++ Buglife.framework/Versions/A/Headers/Buglife.h
@@ -6,26 +6,43 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "LIFEAwesomeLogger.h"
 #import "LIFEInputField.h"
 
+/**
+ Options for automatically invocating the bug reporter view.
+ */
 typedef NS_OPTIONS(NSUInteger, LIFEInvocationOptions) {
+    /// Does not automatically invoke the bug reporter view. Use this if you wish to only manually invoke the bug reporter.
     LIFEInvocationOptionsNone             = 0,
+    /// Invokes the bug reporter by shaking the device (Ctrl+⌘+Z in Simulator).
     LIFEInvocationOptionsShake            = 1 << 0,
+    /// Invokes the bug reporter whenever the user manually takes a screenshot (i.e. by simultaneously pressing the Home & Lock buttons on their device).
     LIFEInvocationOptionsScreenshot       = 1 << 1,
+    /// Places a floating bug button on the screen, which can be moved by the user. Tapping this button invokes the bug reporter.
     LIFEInvocationOptionsFloatingButton   = 1 << 2
 };
 
+/**
+ *  Represents a type of attachment.
+ *
+ *  @see `Buglife.addAttachmentWithData(_:type:filename:error:)`
+ */
 typedef NSString LIFEAttachmentType;
 
+/// Text attachment type.
 extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierText;
+/// JSON attachment type. This can be used to attach network responses, and other JSON payloads.
 extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierJSON;
+/// SQLite attachment type. This can be used to attach Core Data databases, and other SQLite files.
 extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierSqlite;
+/// Image attachment type. This can be used to programmatically attach screenshots. The exact type (JPEG/PNG) will be inferred from the provided filename.
 extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
 
 @protocol BuglifeDelegate;
 
 /**
- *  Buglife. Handles initialization and configuration of Buglife.
+ *  Buglife! Handles initialization and configuration of Buglife.
  */
 @interface Buglife : NSObject
 
@@ -62,7 +79,7 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *  Enables Buglife bug reporting within your app.
  *
  *  The recommended way to enable Buglife is to call this method
- *  in your app delegate's -application:didFinishLaunchingWithOptions: method.
+ *  in your app delegate's `-application:didFinishLaunchingWithOptions:` method.
  *  Don't worry, it won't impact your app's launch performance. 😉
  *
  *  @param apiKey The Buglife API Key for your organization
@@ -76,7 +93,7 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *  up for an account. Bug reports will be sent directly to the provided email.
  *
  *  This method should be called from within your app delegate's
- *  -application:didFinishLaunchingWithOptions: method.
+ *  `-application:didFinishLaunchingWithOptions:` method.
  *
  *  @param email The email address to which bug reports should be sent. This email address should
  *               belong to you or someone on your team.
@@ -104,7 +121,7 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *  app requires users to sign in, then you may wish to use the signed in user's email address
  *  here to identify them when they submit bug reports.
  *
- *  @see userEmailField
+ *  @see `userEmailField`
  *
  *  @param email The current user's email address
  */
@@ -114,20 +131,20 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *  Represents the email address input field in the bug reporter UI.
  *
  *  If your application code cannot programmatically set the user's email
- *  address at runtime via the setUserEmail: method, then you may choose
+ *  address at runtime via the `setUserEmail:` method, then you may choose
  *  to set this field to visible in order to ask the user for their
  *  email address prior to submitting a bug report.
  *
  *  By default, this field is neither visible nor required.
  *
- *  @see setUserEmail:
+ *  @see `setUserEmail(email:)`
  */
 @property (nonatomic, readonly, nonnull) LIFEInputField *userEmailField;
 
 /**
  *  Adds an attachment to be uploaded along with the next bug report.
  *
- *  Although you can add an attachment at any time, it is best to do so within buglife:handleAttachmentRequestWithCompletionHandler:.
+ *  Although you can add an attachment at any time, it is best to do so within `buglife:handleAttachmentRequestWithCompletionHandler:`.
  *  This ensures that your attachment is added once & only once for every submitted bug report.
  *
  *  You may attach up to 10 files, the total size of which may be up to 3 MB.
@@ -136,8 +153,8 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *
  *  This method is thread-safe.
  *
- *  @param attachmentData The attachment data.
- *  @param attachmentType The type of attachment. This must be one of the Buglife-provided LIFEAttachmentType constants.
+ *  @param data The attachment data.
+ *  @param type The type of attachment. This must be one of the Buglife-provided LIFEAttachmentType constants.
  *  @param filename The filename.
  */
 - (BOOL)addAttachmentWithData:(nonnull NSData *)attachmentData type:(nonnull LIFEAttachmentType *)attachmentType filename:(nonnull NSString *)filename error:(NSError * _Nullable * _Nullable)error;
@@ -147,7 +164,7 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *
  *  If necessary, images attached using this method may be automatically resized to fit within the 3 MB total limit for file attachments.
  *
- *  @see addAttachmentWithData:type:filename:error:
+ *  @see `addAttachmentWithData(_:type:filename:)`
  *
  *  @param image The image object to attach.
  *  @param filename The filename.
@@ -158,20 +175,20 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *  Renders & returns a screenshot of your application.
  *
  *  This can be used to generate screenshots to manually attach when
- *  invoking the bug reporter using the `presentReporter` method.
+ *  invoking the bug reporter using the `presentReporter()` method.
  */
 - (nonnull UIImage *)screenshot;
 
 /**
  *  Sorry, Buglife is a singleton 😁
- *  Please use the shared initializer +[Buglife sharedBuglife]
+ *  Please use the shared initializer `Buglife.sharedBuglife()`
  */
 - (nullable instancetype)init NS_UNAVAILABLE;
 
 @end
 
 /**
- *  The BuglifeDelegate protocol provides a mechanism for your application to configure
+ *  The `BuglifeDelegate` protocol provides a mechanism for your application to configure
  *  certain aspects of the Buglife reporter UI.
  */
 @protocol BuglifeDelegate <NSObject>
@@ -181,12 +198,12 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
  *  Buglife calls this method when the bug reporter is ready to accept attachments.
  *
  *  You should use this method to add attachments. Within your method implementation,
- *  use Buglife.addAttachmentWithData(_:type:filename:) to add attachments, then
- *  call the completionHandler. You may both add attachments & call the completionHandler
+ *  use `Buglife.addAttachmentWithData(_:type:filename:)` to add attachments, then
+ *  call the `completionHandler`. You may both add attachments & call the `completionHandler`
  *  on any thread.
  *
- *  @warning You only have a few seconds to add attachments & call the completionHandler.
- *           If the completionHandler isn't called, the bug report submission process
+ *  @warning You only have a few seconds to add attachments & call the `completionHandler`.
+ *           If the `completionHandler` isn't called, the bug report submission process
  *           will continue regardless.
  */
 - (void)buglife:(nonnull Buglife *)buglife handleAttachmentRequestWithCompletionHandler:(nonnull void (^)())completionHandler;
@@ -206,11 +223,11 @@ extern LIFEAttachmentType * __nonnull const LIFEAttachmentTypeIdentifierImage;
 @end
 
 /**
- *  UIView subclasses that contain potentially sensitive information may
+ *  `UIView` subclasses that contain potentially sensitive information may
  *  adopt this protocol so that their contents are automatically blurred
  *  whenever Buglife captures a screenshot.
  *
- *  For example, a UIView subclass for credit card entry should adopt this
+ *  For example, a `UIView` subclass for credit card entry should adopt this
  *  protocol so that a user's credit card is obscured prior to screenshot capturing.
  */
 @protocol LIFEBlurrableView <UICoordinateSpace>
---
+++ Buglife.framework/Versions/A/Headers/LIFEAwesomeLogger.h
@@ -0,0 +1,61 @@
+//
+//  LIFEAwesomeLogger.h
+//  Buglife
+//
+//  Copyright © 2016 Buglife, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define LIFE_Log(isAsync, aType, aFunct, frmt, ...)         \
+    do {                                                    \
+        [[LIFEAwesomeLogger sharedLogger]                   \
+            log:isAsync                                     \
+            type:aType                                      \
+            file:__FILE__                                   \
+            function:aFunct                                 \
+            line:__LINE__                                   \
+            format:(frmt), ## __VA_ARGS__];                 \
+    } while (0)
+
+typedef NS_ENUM(NSUInteger, LIFEAwesomeLogType){
+    LIFEAwesomeLogTypeError    = (1 << 0),
+    LIFEAwesomeLogTypeInfo     = (1 << 2),
+    LIFEAwesomeLogTypeDebug    = (1 << 3)
+};
+
+#define LIFELogError(frmt, ...)     \
+    LIFE_Log(NO, LIFEAwesomeLogTypeError, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+
+#define LIFELogInfo(frmt, ...)     \
+    LIFE_Log(YES, LIFEAwesomeLogTypeInfo, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+
+#define LIFELogDebug(frmt, ...)     \
+    LIFE_Log(YES, LIFEAwesomeLogTypeDebug, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+
+#ifdef LIFE_REPLACE_NSLOG_WITH_AWESOMELOG
+    #define NSLog(frmt, ...) LIFELogDebug(frmt, ##__VA_ARGS__)
+#endif
+
+/**
+ *  Represents the Buglife logging facility.
+ */
+@interface LIFEAwesomeLogger : NSObject
+
++ (nonnull instancetype)sharedLogger;
+
+- (void)log:(BOOL)asynchronous
+       type:(LIFEAwesomeLogType)type
+       file:(nonnull const char *)file
+   function:(nonnull const char *)function
+       line:(NSUInteger)line
+     format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(6,7);
+
+- (void)log:(BOOL)asynchronous
+       type:(LIFEAwesomeLogType)type
+       file:(nonnull NSString *)file
+   function:(nonnull NSString *)function
+       line:(NSUInteger)line
+    message:(nonnull NSString *)message;
+
+@end
